b4ced4cc5fdb9905da23641b8f90f436794a795a
Renildo226 <renildogomes2005@gmail.com>
2026-02-07 12:09:36 +0100

    update

---
+++ Desafios/Contador/script.js
@@ -0,0 +1,31 @@
+/* Variaráveis */
+var ini = document.querySelector('input#ini')
+var fim = document.querySelector('input#fim')
+var pass = document.querySelector('input#pass')
+var res = document.querySelector('div#res')
+function Contar() {
+    var n1 = Number(ini.value)
+    var n2 = Number(fim.value)
+    var passo = Number(pass.value)
+    if (passo <=0) {
+        alert("Passo inválido! Passo deve ser maior que zero!")
+        return
+    }
+    
+    res.innerHTML = `<p>Contando: </p>`
+    for (var c=n1; c<=n2; c+=passo) {
+        res.innerHTML += c+`&#x1F449`
+    }
+    if (ini.value ==='') {
+        alert('Campo vazio!')
+        return
+    }
+    if(fim.value==='') {
+        alert('Campo vazio!')
+        return
+    }
+    if (pass.value==='') {
+        alert('Campo vazio!')
+        return
+    }
+}
