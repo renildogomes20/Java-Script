c4f374f9522ef67c4c277f8988b0fffc7d793bfb
Renildo226 <renildogomes2005@gmail.com>
2026-03-09 12:19:33 +0100

    update

---
+++ CFBCursos/aula029/aula29.js
@@ -0,0 +1,47 @@
+const converterInt=(e)=>parseInt(e)
+const dobrar=(e)=>e*2
+let num=['1','2','3','4','5'].map(converterInt)
+console.log(num)
+
+
+
+
+
+/*const el=document.getElementsByTagName("div")
+const val=Array.prototype.map.call(el,({innerHTML})=>{innerHTML})
+
+console.log(val)
+
+
+/*let el=document.getElementsByTagName("div")
+el=[...el]
+console.log(el)
+el.map((e,i)=>{
+    e.innerHTML="CBF Cursos"  
+})*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*const cursos=['HTML','CSS','Javascript','PHP','React']
+cursos.map((el,i)=>{
+    return "<div>"+el+"</div>"
+})
+
+console.log(c)
+
+/*const notas=[18,17,10,11,16,20]
+
+notas.sort()
+
+notas.map((elem, cont)=>{
+    console.log("Notas: "+elem+" - Posição da nota: "+cont)
+})*/
